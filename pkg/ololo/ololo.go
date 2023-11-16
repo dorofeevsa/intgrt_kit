d2ab80a56111c8e04c2544a4c12026d63ac5da78
Дорофеев Сергей <sdorofeev@astralinux.ru>
2023-11-16 20:10:13 +0200

    test pkg/pkg_name

--- pkg/ololo/ololo.go
+++ pkg/ololo/ololo.go
@@ -9,3 +9,8 @@ func Hello() {
 	logrus.Info("logrus dep")
 	fmt.Println("testing packege hosting")
 }
+
+func HelloTwo() {
+	logrus.Info("logrus dep 2 ")
+	fmt.Println("testing packege hosting 2 ")
+}
